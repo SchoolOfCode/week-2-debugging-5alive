5d64fff6c9cae1e93657364242ae2172924191b4
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2024-12-04 15:31:03 +0000

    Ticket 5 complete

--- ticket-5/index.js
+++ ticket-5/index.js
@@ -19,9 +19,9 @@ const shoppingCart = [
 
 let totalCost = 0;
 
-for (let i = 0; i <= shoppingCart.length; i++) {
-  const item = shoppingCart[i];
-  const costForItem = item.price * item.quantity;
+for (let i = 0; i < shoppingCart.length; i++) {
+  let item = shoppingCart[i];
+  let costForItem = item.price * item.quantity;
   totalCost += costForItem;
 }
 
